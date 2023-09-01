1ed1c0e918fabe878b901ca644b50cf13d149077
Siddharth Pandey <sidd@Siddharths-MacBook-Pro.local>
2023-09-01 19:00:32 +0530

    Initial commit

---
+++ current/app.py
@@ -0,0 +1,30 @@
+import yfinance as yf
+from flask import request, render_template, jsonify, Flask
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/get_stock_data', methods=['POST'])
+def get_stock_data():
+    try:
+        ticker = request.get_json()['ticker']
+        data = yf.Ticker(ticker).history(period='1y')  # Use the 'ticker' variable, not the string 'ticker'
+
+        if data.empty:
+            return jsonify({'error': 'No data available for this ticker'}), 400
+
+        return jsonify({'current_price': data.iloc[-1].Close, 'open_price': data.iloc[-1].Open})
+
+    except KeyError:
+        return jsonify({'error': 'Invalid request data'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ current/static/script.js
@@ -0,0 +1,93 @@
+var ticker = JSON.parse(localStorage.getItem('tickers')) || [];
+var lastPrices = {};
+var counter = 15;
+
+function startUpdateCycle() {
+    updatePrices();
+    var countdown = setInterval(function () {
+        counter--;
+        $('#counter').text(counter);
+        if (counter <= 0) {
+            updatePrices();
+            counter = 15;
+        }
+    }, 1000);
+}
+
+$(document).ready(function () {
+    ticker.forEach(function (ticker) {
+        addTickerToGrid(ticker);
+    });
+
+    updatePrices();
+
+    $('#add-ticker-form').submit(function (e) {
+        e.preventDefault();
+        var newTicker = $('#new-ticker').val().toUpperCase();
+        if (!ticker.includes(newTicker)) {
+            ticker.push(newTicker);
+            localStorage.setItem('tickers', JSON.stringify(ticker))
+            addTickerToGrid(newTicker);
+        }
+        $('#new-ticker').val('');
+        updatePrices();
+    });
+
+    $('#tickers-grid').on('click', '.remove-btn', function () {
+        var tickerToRemove = $(this).data('ticker');
+        ticker = ticker.filter(t => t !== tickerToRemove);
+        localStorage.setItem('tickers', JSON.stringify(ticker))
+        $(`#${tickerToRemove}`).remove();
+    });
+
+    startUpdateCycle();
+});
+
+function addTickerToGrid(ticker) {
+    $('#tickers-grid').append(`<div id="${ticker}" class="stock-box"><h2>${ticker}</h2><p id="${ticker}-price"></p><p id="${ticker}-pct"></p><button class="remove-btn" data-ticker="${ticker}">Remove</button></div>`)
+}
+
+function updatePrices() {
+    ticker.forEach(function (ticker) {
+        $.ajax({
+            url: '/get_stock_data',
+            type: 'POST',
+            data: JSON.stringify({'ticker': ticker}),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                var changePercent = ((data.current_price - data.open_price) / data.open_price) * 100;
+                var colorClass;
+                if (changePercent <= -2) {
+                    colorClass = 'dark-red'
+                } else if (changePercent <= 0) {
+                    colorClass = 'gray'
+                } else if (changePercent <=2) {
+                    colorClass = 'green'
+                } else {
+                    colorClass = 'dark-green'
+                }
+
+                $(`#${ticker}-price`).text(`$${data.current_price.toFixed(2)}`);
+                $(`#${ticker}-pct`).text(`${changePercent.toFixed(2)}%`);
+                $(`#${ticker}-price`).removeClass('dark-red gray green dark-green').addClass(colorClass);
+                $(`#${ticker}-pct`).removeClass('dark-red gray green dark-green').addClass(colorClass);
+
+                var flashClass;
+                if (lastPrices[ticker] > data.current_price) {
+                    flashClass = 'red-flash';
+                } else if (lastPrices[ticker] < data.current_price) {
+                    flashClass = 'green-flash';
+                } else {
+                    flashClass = 'gray-flash';
+                }
+                lastPrices[ticker] = data.current_price;
+
+                $(`#${ticker}`).addClass(flashClass);
+                setTimeout(function () {
+                    $(`#${ticker}`).removeClass(flashClass);
+                }, 1000);
+            }
+        })
+    })
+}
